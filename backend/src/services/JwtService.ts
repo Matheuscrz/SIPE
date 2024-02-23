c65b7699fbaf5adf9fba85bee5ac9dded89a94a9
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-02-23 11:37:38 -0300

    Ajustes na geração dos tokens - Incompleto

--- backend/src/services/JwtService.ts
+++ backend/src/services/JwtService.ts
@@ -1,19 +1,15 @@
 import jwt, { Secret, SignOptions, VerifyOptions } from "jsonwebtoken";
 import { RedisCache } from "../config/Redis";
 import { TokenRevocationController } from "../controllers/TokenRevocationController";
-import { User } from "../interfaces/User";
+import { User as UserType } from "../interfaces/User";
 
-export class JwtService {
+export default class JwtService {
   private static readonly secretKey: Secret =
-    process.env.JWT_SECRET || "secret-key";
+    process.env.JWT_SECRET_KEY || "secret";
   private static readonly algorithm: string = "HS256";
   private static readonly issuer: string = "SIPE";
 
-  private static generateToken(
-    user: User,
-    expiresIn: string,
-    cacheKey: string
-  ): string {
+  private static generateToken(user: UserType, expiresIn: string): string {
     const payload = {
       id: user.id,
       personalData: user.personalData,
@@ -28,125 +24,18 @@ export class JwtService {
     };
 
     const token = jwt.sign(payload, this.secretKey, options);
-
-    RedisCache.setex(
-      cacheKey,
-      parseInt(expiresIn) + 60,
-      JSON.stringify(payload)
-    );
-
     return token;
   }
 
-  static generateAccessToken(user: User): string {
-    return this.generateToken(user, "15m", `access-token:${user.id}`);
+  static generateAccessToken(user: UserType): string {
+    return this.generateToken(user, "15m");
   }
 
-  static generateRefreshToken(user: User): string {
-    return this.generateToken(user, "7d", `refresh-token:${user.id}`);
+  static generateRefreshToken(user: UserType): string {
+    return this.generateToken(user, "7d");
   }
 
-  static async verifyAccessToken(
-    token: string
-  ): Promise<User | null | undefined> {
-    const cachedUser = await RedisCache.get(`access-token:${token}`);
-    if (cachedUser) {
-      return JSON.parse(cachedUser);
-    }
-
-    const isTokenRevoked = await TokenRevocationController.isTokenRevoked(
-      token
-    );
-    if (isTokenRevoked) {
-      return null;
-    }
-
-    try {
-      const options: VerifyOptions = {
-        algorithms: [this.algorithm as jwt.Algorithm],
-        issuer: this.issuer,
-      };
-
-      const payload = jwt.verify(token, this.secretKey, options) as User;
-
-      RedisCache.setex(
-        `access-token:${payload.id}`,
-        900,
-        JSON.stringify(payload)
-      );
-
-      return payload;
-    } catch (error) {
-      console.error("Erro durante verificação de token de acesso:", error);
-      return null;
-    }
-  }
-
-  static async verifyRefreshToken(
-    token: string
-  ): Promise<User | null | undefined> {
-    const cachedUser = await RedisCache.get(`refresh-token:${token}`);
-    if (cachedUser) {
-      return JSON.parse(cachedUser);
-    }
-
-    const isTokenRevoked = await TokenRevocationController.isTokenRevoked(
-      token
-    );
-    if (isTokenRevoked) {
-      return null;
-    }
-
-    try {
-      const options: VerifyOptions = {
-        algorithms: [this.algorithm as jwt.Algorithm],
-        issuer: this.issuer,
-      };
-
-      const payload = jwt.verify(token, this.secretKey, options) as User;
-
-      RedisCache.setex(
-        `refresh-token:${payload.id}`,
-        604800,
-        JSON.stringify(payload)
-      );
-
-      return payload;
-    } catch (error) {
-      console.error("Erro durante verificação de token de atualização:", error);
-      return null;
-    }
-  }
-
-  static decodeToken(token: string): { [key: string]: any } | null {
-    try {
-      const decoded = jwt.decode(token);
-      return decoded ? (decoded as { [key: string]: any }) : null;
-    } catch (error) {
-      console.error("Erro durante decodificação de token:", error);
-      return null;
-    }
-  }
-
-  static async refreshTokens(
-    refreshToken: string
-  ): Promise<{ accessToken: string; refreshToken: string } | null> {
-    try {
-      const user = await JwtService.verifyRefreshToken(refreshToken);
-
-      if (user) {
-        const newAccessToken = JwtService.generateAccessToken(user);
-
-        return {
-          accessToken: newAccessToken,
-          refreshToken,
-        };
-      } else {
-        return null;
-      }
-    } catch (error) {
-      console.error("Erro durante renovação de tokens:", error);
-      return null;
-    }
-  }
+  //   static verifyToken(token: string): Promise<UserType | null | undefined> {
+  //     const cachedUser = await RedisCache.get(token);
+  //   }
 }
