7486820719ab6ef1cf8a5892cfe95ba5fcc8953d
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-02-23 11:36:27 -0300

    Ajustes no acesso ao banco e nos metodos de acesso

--- backend/src/models/UserModel.ts
+++ backend/src/models/UserModel.ts
@@ -1,72 +1,64 @@
 import { Database } from "../config/Database";
-import { User } from "../interfaces/User";
+import { RedisCache } from "../config/Redis";
+import { User as UserType } from "../interfaces/User";
 
-export class UserModel {
-  private static readonly TABLE_NAME = "point.employees";
+export default class UserModel {
+  private static readonly TABLE_USER = "point.employees";
+  private static readonly TABLE_REFRESH_TOKEN = "point.login_tokens";
 
   /**
-   * Obtém um usuário pelo CPF.
-   * @param cpf - CPF do usuário.
-   * @returns Objeto User ou null se o usuário não for encontrado.
+   * Obtém um usuário por meio do CPF
+   * @param cpf - CPF do usuário
+   * @returns Objeto User ou null se não encontrar
    */
-  static async getUserByCpf(cpf: string): Promise<User | null> {
-    const query = `SELECT * FROM point.employees WHERE cpf = $1`;
+  static async getByCpf(cpf: string): Promise<UserType | null> {
+    const query = `SELECT * FROM ${this.TABLE_USER} WHERE cpf = $1`;
     const values = [cpf];
     try {
-      const client = await Database.getPool().connect();
-      const result = await client.query(query, values);
+      const result = await Database.query(query, values);
       const user = result.rows.length ? result.rows[0] : null;
-      client.release();
       return user;
     } catch (error) {
-      throw new Error(
-        `Erro ao buscar usuário por CPF: ${(error as Error).message}`
-      );
+      throw new Error(`Erro ao buscar usuário por CPF: ${error}`);
     }
   }
 
   /**
    * Atualiza as informações de login do usuário no banco de dados.
-   * @param userId - ID do usuário.
-   * @param loginAttempts - Número de tentativas de login.
-   * @returns Promise<void>
+   * @param id - ID do usuário
+   * @param loginAttempt - Número de tentativas de login
    */
-  static async updateLoginAttempts(
-    userId: number,
-    loginAttempts: number
+  static async updateLoginAttempt(
+    id: string,
+    loginAttempt: number
   ): Promise<void> {
-    const query = `UPDATE ${this.TABLE_NAME} SET login_attempts = $1 WHERE id = $2`;
-    const values = [loginAttempts, userId];
+    const query = `UPDATE ${this.TABLE_USER} SET login_attempt = $1 WHERE id = $2`;
+    const values = [loginAttempt, id];
     try {
       await Database.query(query, values);
     } catch (error) {
-      throw new Error(
-        `Erro ao buscar usuário por CPF: ${(error as Error).message}`
-      );
+      throw new Error(`Erro ao atualizar tentativa de login: ${error}`);
     }
   }
 
   /**
-   * Armazena o token de refresh no banco de dados.
-   * @param userId - ID do usuário.
-   * @param refreshToken - Token de refresh.
-   * @param expiresAt - Data de expiração do token de refresh.
-   * @returns Promise<void>
+   * Armazena o token refresh no banco de dados e no cache.
+   * @param id - ID do usuário
+   * @param refreshToken - Token refresh
+   * @param expiresAt - Data de expiração do token
    */
   static async storeRefreshToken(
-    userId: string,
+    id: string,
     refreshToken: string,
     expiresAt: Date
   ): Promise<void> {
-    const query =
-      "INSERT INTO login_tokens (user_id, refresh_token, expires_at) VALUES ($1, $2, $3)";
-    const values = [userId, refreshToken, expiresAt];
+    const query = `INSERT INTO ${this.TABLE_REFRESH_TOKEN} (user_id, token, expires_at) VALUES ($1, $2, $3)`;
+    const values = [id, refreshToken, expiresAt];
     try {
       await Database.query(query, values);
+      await RedisCache.setex(id, 604800, refreshToken);
     } catch (error) {
-      throw new Error(
-        `Erro ao buscar usuário por CPF: ${(error as Error).message}`
-      );
+      throw new Error(`Erro ao armazenar token refresh: ${error}`);
     }
   }
 }
