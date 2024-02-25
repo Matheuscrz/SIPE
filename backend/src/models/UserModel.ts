1e77e72d0b1be4336385c3723e54b0180d249c1c
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-02-25 11:15:57 -0300

    Ajustes na UserModel devido a mudanças nos metodos da classe Rediscache

--- backend/src/models/UserModel.ts
+++ backend/src/models/UserModel.ts
@@ -5,6 +5,7 @@ import { User as UserType } from "../interfaces/User";
 export default class UserModel {
   private static readonly TABLE_USER = "point.employees";
   private static readonly TABLE_REFRESH_TOKEN = "point.login_tokens";
+  private static readonly TABLE_REVOKED_TOKEN = "point.revoked_tokens";
 
   /**
    * Obtém um usuário por meio do CPF
@@ -24,24 +25,6 @@ export default class UserModel {
   }
 
   /**
-   * Atualiza as informações de login do usuário no banco de dados.
-   * @param id - ID do usuário
-   * @param loginAttempt - Número de tentativas de login
-   */
-  static async updateLoginAttempt(
-    id: string,
-    loginAttempt: number
-  ): Promise<void> {
-    const query = `UPDATE ${this.TABLE_USER} SET login_attempt = $1 WHERE id = $2`;
-    const values = [loginAttempt, id];
-    try {
-      await Database.query(query, values);
-    } catch (error) {
-      throw new Error(`Erro ao atualizar tentativa de login: ${error}`);
-    }
-  }
-
-  /**
    * Armazena o token refresh no banco de dados e no cache.
    * @param id - ID do usuário
    * @param refreshToken - Token refresh
@@ -56,9 +39,51 @@ export default class UserModel {
     const values = [id, refreshToken, expiresAt];
     try {
       await Database.query(query, values);
-      await RedisCache.setex(id, 604800, refreshToken);
+      await RedisCache.set(id, refreshToken);
     } catch (error) {
       throw new Error(`Erro ao armazenar token refresh: ${error}`);
+    }
+  }
+
+  /**
+   *  Armazena o token de acesso no cache.
+   * @param id - ID do usuário
+   * @param accessToken - Token de acesso
+   * @param expiresAt - Data de expiração do token
+   */
+  static async storeAccessToken(
+    id: string,
+    accessToken: string
+  ): Promise<void> {
+    try {
+      await RedisCache.set(id, accessToken);
+    } catch (error) {
+      throw new Error(`Erro ao armazenar token de acesso: ${error}`);
+    }
+  }
+
+  /**
+   * Remove o token refresh do banco de dados e do cache.
+   * @param refreshToken - Token refresh
+   * @returns True se o token foi removido, false se não encontrou
+   */
+  static async removeRefreshToken(refreshToken: string): Promise<boolean> {
+    const deleteQuery = `DELETE FROM ${this.TABLE_REFRESH_TOKEN} WHERE refresh_token = $1 RETURNING id, expires_at`;
+    const values = [refreshToken];
+    try {
+      const result = await Database.query(deleteQuery, values);
+      if (result.rows.length > 0) {
+        const { id, expires_at } = result.rows[0];
+        const insertRevokedQuery = `INSERT INTO ${this.TABLE_REVOKED_TOKEN} (id, token, expires_at) VALUES ($1, $2, $3)`;
+        const revokedTokenValues = [id, refreshToken, expires_at];
+        await Database.query(insertRevokedQuery, revokedTokenValues);
+        await RedisCache.del(id);
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      throw new Error(`Erro ao remover token refresh: ${error}`);
     }
   }
 }
