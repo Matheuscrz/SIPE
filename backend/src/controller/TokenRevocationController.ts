02bcaf87f7c8863d6afb7890018eaaa108aadfe4
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-02-25 11:15:05 -0300

    Refatoração e ajustes nos metodos de revogação e verificação de tokens

--- backend/src/controller/TokenRevocationController.ts
+++ backend/src/controller/TokenRevocationController.ts
@@ -5,43 +5,42 @@ import { Database } from "../config/Database";
  * Classe de controle para revogação de tokens
  */
 export class TokenRevocationController {
+  private static readonly TABLE_REFRESH_TOKEN = "point.login_tokens";
+  private static readonly TABLE_REVOKED_TOKEN = "point.revoked_tokens";
+
   /**
    *Verifica se o token está na lista de tokens revogados
+   * @param id - id do usuário a quem o token pertence
    * @param token - token a ser verificado
    * @returns true se o token estiver na lista de tokens revogados, false caso contrário
    */
-  static async isTokenRevoked(token: string): Promise<boolean> {
-    const cachedRevoked = await RedisCache.get(`revogado-token:${token}`);
-    if (cachedRevoked) {
-      return true;
-    } else {
-      try {
-        const result = await Database.query(
-          "SELECT * FROM tokens_revogados WHERE token = $1",
-          [token]
-        );
-        if (result.rows.length > 0) {
-          await RedisCache.setex(`revogado-token:${token}`, 604800, "true");
-          return true;
-        }
+  static async isTokenRevoked(id: string, token: string): Promise<boolean> {
+    const query = `SELECT * FROM ${this.TABLE_REVOKED_TOKEN} WHERE token = $1`;
+    try {
+      const result = await Database.query(query, [token]);
+      if (result.rows.length > 0) {
+        return true;
+      } else {
         return false;
-      } catch (error) {
-        console.error("Erro ao verificar se o token está revogado: ", error);
-        throw error;
       }
+    } catch (error) {
+      console.error("Erro ao verificar token revogado: ", error);
+      throw error;
     }
   }
   /**
    * Adiciona um token à lista de tokens revogados e ao cache
+   * @param id - id do usuário a quem o token pertence
    * @param token - token a ser revogado
    * @returns {Promise<void>}
    */
-  static async revokeToken(token: string): Promise<void> {
+  static async revokeToken(id: string, token: string): Promise<void> {
+    const deleteQuery = `DELETE FROM ${this.TABLE_REFRESH_TOKEN} WHERE user_id = $1 AND token = $2`;
+    const insertQuery = `INSERT INTO ${this.TABLE_REVOKED_TOKEN} token VALUES ($1)`;
     try {
-      await Database.query("INSERT INTO tokens_revogados (token) VALUES ($1)", [
-        token,
-      ]);
-      await RedisCache.setex(`revogado-token:${token}`, 604800, "true");
+      await Database.query(deleteQuery, [id, token]);
+      await Database.query(insertQuery, [token]);
+      await RedisCache.set(id, token);
     } catch (error) {
       console.error("Erro ao revogar token: ", error);
       throw error;
