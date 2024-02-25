19bfa2339c202f6d4f527880dccf3fe1b6d7146d
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-02-25 12:02:26 -0300

    feat: Implementação de geração e verificação de tokens

--- backend/src/services/JwtService.ts
+++ backend/src/services/JwtService.ts
@@ -1,15 +1,35 @@
 import jwt, { Secret, SignOptions, VerifyOptions } from "jsonwebtoken";
-import { RedisCache } from "../config/Redis";
 import { TokenRevocationController } from "../controller/TokenRevocationController";
 import { User as UserType } from "../interfaces/User";
 
 export default class JwtService {
   private static readonly secretKey: Secret =
-    process.env.JWT_SECRET_KEY || "secret";
+    process.env.JWT_SECRET_KEY_REFRESH || "secret-key";
   private static readonly algorithm: string = "HS256";
   private static readonly issuer: string = "SIPE";
 
-  private static generateToken(user: UserType, expiresIn: string): string {
+  /**
+   * Gera um token de acesso
+   * @param refreshToken - token de atualização
+   * @returns token de acesso
+   */
+  private static async generateAccessToken(refreshToken): Promise<string> {
+    const payload = this.decodeToken(refreshToken);
+    const options: SignOptions = {
+      algorithm: this.algorithm as jwt.Algorithm,
+      expiresIn: "15m",
+      issuer: this.issuer,
+      subject: payload.id,
+    };
+    return jwt.sign(payload, refreshToken, options);
+  }
+
+  /**
+   * Gera um token de atualização
+   * @param user - usuário a quem o token pertence
+   * @returns token de atualização
+   */
+  private static async generateRefreshToken(user: UserType): Promise<string> {
     const payload = {
       id: user.id,
       personalData: user.personalData,
@@ -17,25 +37,81 @@ export default class JwtService {
       permissions: user.permissions,
       iss: this.issuer,
     };
-
     const options: SignOptions = {
-      expiresIn,
       algorithm: this.algorithm as jwt.Algorithm,
+      expiresIn: "7d",
+      issuer: this.issuer,
+      subject: user.id,
     };
-
-    const token = jwt.sign(payload, this.secretKey, options);
-    return token;
+    return jwt.sign(payload, this.secretKey, options);
   }
 
-  static generateAccessToken(user: UserType): string {
-    return this.generateToken(user, "15m");
+  /**
+   * Decodifica um token
+   * @param token - token a ser decodificado
+   * @returns - retorna o payload do token
+   */
+  private static decodeToken(token: string): UserType {
+    return jwt.decode(token) as UserType;
   }
 
-  static generateRefreshToken(user: UserType): string {
-    return this.generateToken(user, "7d");
+  /**
+   * Verifica se um token é válido
+   * @param token - token a ser verificado
+   * @returns true se o token for válido, false caso contrário
+   */
+  private static async verifyRefreshToken(token: string): Promise<boolean> {
+    const options: VerifyOptions = {
+      algorithms: [this.algorithm as jwt.Algorithm],
+      issuer: this.issuer,
+    };
+    try {
+      const decoded = jwt.verify(token, this.secretKey, options);
+      const id = (decoded as UserType).id;
+      if (await TokenRevocationController.isTokenRevoked(id, token)) {
+        return false;
+      } else {
+        return true;
+      }
+    } catch (error) {
+      console.error("Erro ao verificar token: ", error);
+      return false;
+    }
   }
 
-  //   static verifyToken(token: string): Promise<UserType | null | undefined> {
-  //     const cachedUser = await RedisCache.get(token);
-  //   }
+  /**
+   * Verifica se um token é válido
+   * @param token - token a ser verificado
+   * @param refreshToken - token de atualização
+   * @returns - true se o token for válido, false caso contrário
+   */
+  private static async verifyAccessToken(
+    token: string,
+    refreshToken: string
+  ): Promise<boolean> {
+    const options: VerifyOptions = {
+      algorithms: [this.algorithm as jwt.Algorithm],
+      issuer: this.issuer,
+    };
+    try {
+      jwt.verify(token, refreshToken, options);
+      return true;
+    } catch (error) {
+      console.error("Erro ao verificar token: ", error);
+      return false;
+    }
+  }
+
+  /**
+   * Gera um token de acesso e um token de atualização
+   * @param user - usuário a quem os tokens pertencem
+   * @returns - um objeto contendo o token de acesso e o token de atualização
+   */
+  private static async generateTokens(
+    user: UserType
+  ): Promise<{ accessToken: string; refreshToken: string }> {
+    const refreshToken = await this.generateRefreshToken(user);
+    const accessToken = await this.generateAccessToken(refreshToken);
+    return { accessToken, refreshToken };
+  }
 }
