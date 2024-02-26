46987efca71e1c39c3ed7201cab22891c8a9dc66
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-02-26 11:37:31 -0300

    Classe responsável por criar e iniciar o servidor

---
+++ backend/src/App.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { Routes } from "./router/Routes";
+
+export class App {
+  private readonly app: express.Application;
+  private readonly port: number;
+
+  constructor(port: number) {
+    this.app = express();
+    this.port = port;
+    this.configureApp();
+  }
+
+  private configureApp(): void {
+    const routes = new Routes(this.app);
+    this.app.use(routes.getRouter());
+  }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.log(`Servidor escutando na porta: ${this.port}`);
+    });
+  }
+}
+
+const port = 3000;
+const application = new App(port);
+application.start();
