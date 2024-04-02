a2e695b25aeb76cdfcac890bd4bde1b40b3c7848
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2024-04-02 11:08:46 -0300

    Ajuste nas interfaces que representam estruturas do banco

--- backend/src/interfaces/Role.ts
+++ backend/src/interfaces/Role.ts
@@ -3,7 +3,6 @@
  * @description Interface para Role que representa a estrutura de um cargo no banco de dados
  */
 export interface Role {
-  id: string;
   name: string;
   description: string;
   created_at: string;
